e19edf542404d62aa392bf711879f6d76ffbfca3
zg9064 <zgoldstein1@hwemail.com>
2017-02-18 10:15:29 -0800

    Update pictures.java

--- pictures/pictures.java
+++ pictures/pictures.java
@@ -1,3 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.awt.image.*;
+
+import javax.imageio.*;
+import javax.imageio.stream.ImageOutputStream;
+
 public static void main(String[]args){
   
 }
