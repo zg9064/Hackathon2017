030981f10e624767bfcdd1641aca5f1a6c12b2d6
zg9064 <zgoldstein1@hwemail.com>
2017-02-18 11:10:34 -0800

    Update pictures.java

--- pictures/pictures.java
+++ pictures/pictures.java
@@ -1,26 +1,40 @@
 import java.io.*;
 import java.util.*;
+import java.awt.Color;
 import java.awt.image.*;
+
 import javax.imageio.*;
 import javax.imageio.stream.ImageOutputStream;
-public static void main(String[]args){
-  BufferedImage img = ImageIO.read(new File(IMAGE NAME, FIND OUT HOW TO PULL IT INTO THE FILE));
-  int width=image.getWidth();
-  int height=image.getHeight();
-  int[] hues=new int[361];
-  int black=0;
-  int white=0;
-  for(int r=0;r<height;r++){
-    for(int c=0;c<width;c++){
-      int rgb=img.getRGB(c,r);
-      float hsb[] = new float[3];
-      int r = (rgb >> 16) & 0xFF;
-      int g = (rgb >>  8) & 0xFF;
-      int b = (rgb      ) & 0xFF;
-      Color.RGBtoHSB(r, g, b, hsb);
-      if      (hsb[1] < 0.1 && hsb[2] > 0.9) white++;
-      else if (hsb[2] < 0.1                ) black++;
-      else                                   hues[hsb[0]*360]++;
-    }
-  }
+@SuppressWarnings("all")
+public class picTest {
+	public static void main(String[]args) throws IOException{
+		BufferedImage image = ImageIO.read(new File(INPUT));
+		int width=image.getWidth();
+		int height=image.getHeight();
+		int[] hues=new int[360];
+		int black=0;
+		int white=0;
+		for(int y=0;y<height;y++){
+			for(int x=0;x<width;x++){
+				int rgb=image.getRGB(x,y);
+				float hsb[] = new float[3];
+				int r = (rgb >> 16) & 0xFF;
+				int g = (rgb >>  8) & 0xFF;
+				int b = (rgb      ) & 0xFF;
+				Color.RGBtoHSB(r, g, b, hsb);
+				if      (hsb[1] < 0.1 && hsb[2] > 0.9) white++;
+				else if (hsb[2] < 0.1                ) black++;
+				else                                   hues[(int) (hsb[0]*360)]++;
+			}
+		}
+    /*
+		String s="[";
+		for(int i=0; i<360;i++)
+			if(i==359)
+				s+=hues[i]+"]";
+			else
+				s+=hues[i]+", ";
+		System.out.println("black: "+black+" white: "+white+" hues: "+s);
+    */
+	}
 }
