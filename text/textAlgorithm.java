c025e3fcb9d9d704c31421948f76fc6e3562c3fb
Jring11 <jring1@hwemail.com>
2017-02-18 11:42:35 -0800

    Update textAlgorithm.java

--- text/textAlgorithm.java
+++ text/textAlgorithm.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.scanner;
 public class textAlgorithm {
   public static void main(String[] args) {
-    private static String[] uselessWords = {"he", "her", "I", "you", "them", "us", "we", "it", "to"};
+    private static String[] uselessWords = {"he", "her", "I", "you", "them", "us", "we", "it", "to", "a", "and", "the", "of", "at", "from", "in", "on", "my", "your", "his", "its", "our", "their", "one's", "mine", "yours", "hers", "ours", "theirs", "she", "they", "one"};
   
   }
 }
