198ea32e20e5847ed184b886c187a1a600f97702
Jring11 <jring1@hwemail.com>
2017-02-18 12:49:58 -0800

    Update textAlgorithm.java

--- text/textAlgorithm.java
+++ text/textAlgorithm.java
@@ -3,7 +3,7 @@ import java.util.scanner;
 import java.io.file;
 public class textAlgorithm {
   public static void main(String[] args) {
-    private static String[] uselessWords = {"he", "her", "I", "you", "them", "us", "we", "it", "to", "a", "and", "the", "of", "at", "from", "in", "on", "my", "your", "his", "its", "our", "their", "one's", "mine", "yours", "hers", "ours", "theirs", "she", "they", "one"};
+    private static String[] uselessWords = {"he", "her", "I", "you", "them", "us", "we", "it", "to", "a", "and", "the", "of", "at", "from", "in", "on", "my", "your", "his", "its", "our", "their", "one's", "mine", "yours", "hers", "ours", "theirs", "she", "they", "one", "with"};
     
     public static void removeWords(string stringName){
       for(int i = 0; i<uselessWords.length; i++)
