09dc18691158917759fdc74e3a298e7ab1668394
santillan64 <jason_santillan@hotmail.com>
2017-02-18 11:25:43 -0800

    Update textAlgorithm.java

--- text/textAlgorithm.java
+++ text/textAlgorithm.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.scanner;
 public class textAlgorithm {
+  public static void main(String[] args) {
   private static ArrayList<String> uselessWords;
   
+  }
 }
