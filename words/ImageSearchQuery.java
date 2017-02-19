0db474b58eb535f062022ea80d9d058683c2494d
AGGGGG <agaut1@hwemail.com>
2017-02-19 10:58:06 -0800

    Update ImageSearchQuery.java

--- words/ImageSearchQuery.java
+++ words/ImageSearchQuery.java
@@ -1,11 +1,13 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import javax.lang.model.util.Elements;
@@ -22,61 +24,69 @@ import org.w3c.dom.NodeList;
 
 public class TestImage {
 
-	public static void main (String [] args) throws org.json.simple.parser.ParseException, JSONException {
+	public static void main (String [] args) throws org.json.simple.parser.ParseException, JSONException, IOException {
+		File file = new File ("URLStrings.txt");
+		FileWriter fw = new FileWriter (file);
+		PrintWriter pw = new PrintWriter(fw);
 
-		String test = "test"; //will be whatever is passed from other class
-		String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
-		String url = "https://www.google.com/search?site=imghp&tbm=isch&source=hp&q=" + test;
+		int counter5=0;
+		Scanner scan = new Scanner (new File ("maxFrequencyWords.txt"));
+		while (scan.hasNext()) {
+			String word = scan.next(); 
+			String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+			String url = "https://www.google.com/search?site=imghp&tbm=isch&source=hp&q=" + word;
 
-		ArrayList<String> resultUrls = new ArrayList<String>();
-		//ArrayList<Element> elements = new ArrayList<Element>();
+			//ArrayList<String> resultUrls = new ArrayList<String>();
+			//ArrayList<Element> elements = new ArrayList<Element>();
 
-		try {
-			org.jsoup.nodes.Document doc = Jsoup.connect(url).userAgent(userAgent).referrer("https://www.google.com/").get();
+			try {
+				org.jsoup.nodes.Document doc = Jsoup.connect(url).userAgent(userAgent).referrer("https://www.google.com/").get();
 
-			///org.w3c.dom.NodeList elements = (org.w3c.dom.NodeList) (doc).select("div.rg_meta");
-			org.jsoup.select.Elements elements = (org.jsoup.select.Elements) (doc).select("div.rg_meta");
+				///org.w3c.dom.NodeList elements = (org.w3c.dom.NodeList) (doc).select("div.rg_meta");
+				org.jsoup.select.Elements elements = (org.jsoup.select.Elements) (doc).select("div.rg_meta");
 
-			JSONObject jsonObject;
-			int counter = 0;
-			for (Node element : elements) {
-				if (counter == 5) {
-					break;
+				JSONObject jsonObject;
+				int counter = 0;
+				for (Node element : elements) {
+					if (counter == 5) {
+						counter5++;
+						break;
+					}
+					if (((Node) element).childNodeSize() > 0) {
+						jsonObject = (JSONObject) new JSONParser().parse(((Node) element).childNode(0).toString());
+						//resultUrls.add((String) jsonObject.get("ou"));
+						//pw.println((String)jsonObject.get("ou"));
+						if(counter5==4&&counter==4) pw.print((String)jsonObject.get("ou"));
+						else pw.println((String)jsonObject.get("ou"));
+					}
+					counter++;
 				}
-				if (((Node) element).childNodeSize() > 0) {
-					jsonObject = (JSONObject) new JSONParser().parse(((Node) element).childNode(0).toString());
-					resultUrls.add((String) jsonObject.get("ou"));
-				}
-				counter++;
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println("number of results: " + resultUrls.size());
-			
-			File file = new File ("URLStrings.txt");
-			FileWriter fw = new FileWriter (file);
-			PrintWriter pw = new PrintWriter(fw);
 
-			int counter2 = 0;
-			for (String imageUrl : resultUrls) {
-				counter2++;
-				System.out.println(imageUrl);
-				pw.print(imageUrl);
-				if (counter2 != 5) {
-					pw.print("\n");
-				}
-			}
-			
-			fw.close();
-			pw.close();
-			
-			
-			
-//			for (String imageUrl : resultUrls) {
-//				 BufferedImage image = ImageIO.read(new URL(imageUrl));
-//				 System.out.println(image);
-//			}
+			//System.out.println("number of results: " + resultUrls.size());
 
-		} catch (IOException e) {
-			e.printStackTrace();
+
+			//			int counter2 = 0;
+			//			for (String imageUrl : resultUrls) {
+			//				counter2++;
+			//				System.out.println(imageUrl);
+			//				pw.print(imageUrl);
+			//				if (counter2 != 25) {
+			//					pw.print("\n");
+			//				}
 		}
+		fw.close();
+		pw.close();
+
+
+
+		//			for (String imageUrl : resultUrls) {
+		//				 BufferedImage image = ImageIO.read(new URL(imageUrl));
+		//				 System.out.println(image);
+		//			}
+
+
 	}
 }
